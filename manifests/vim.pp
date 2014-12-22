7d0af4dd918f8697bca161da6a452030d895ddd2
Thomas Vander Stichele <thomasvs (at) google (dot) com>
2014-12-22 17:04:33 -0500

    allow specifying a vimrc_source

--- manifests/vim.pp
+++ manifests/vim.pp
@@ -1,9 +1,10 @@
-# = Define: vim
+# = Define: user::vim
 #
 # This define sets up the .vim hierarchy in the home directory.
 define user::vim (
   $user = $title,
   $group = $title,
+  $vimrc_source = undef,
 ) {
 
   $home = user_home($user)
@@ -17,5 +18,12 @@ define user::vim (
     owner  => $user,
     group  => $group,
     mode   => '0750',
+  }
+
+  if ($vimrc_source) {
+    file { "${home}/.vimrc":
+      ensure => present,
+      source => $vimrc_source,
+    }
   }
 }
