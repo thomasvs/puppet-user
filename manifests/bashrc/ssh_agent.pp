54b42fc84abd9d0c127bd3326df48834c2054818
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2015-04-06 14:06:16 -0700

    add ssh-agent running to .bashrc.d

---
+++ manifests/bashrc/ssh_agent.pp
@@ -0,0 +1,20 @@
+# = Define: user::bashrc::ssh_agent
+#
+# This define deploys a .bashrc.d snippet to use a session ssh-agent
+#
+define user::bashrc::ssh_agent (
+  $user = $title,
+  $group = $title,
+) {
+
+  $home = user_home($user)
+
+  file { "${home}/.bashrc.d/ssh-agent":
+    ensure  => present,
+    owner   => $user,
+    group   => $group,
+    mode    => '0640',
+    source  => 'puppet:///modules/user/bashrc.d/ssh-agent',
+    require => User::Bashrc[$user],
+  }
+}
